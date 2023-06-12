a4d07bd27b9d94d3c02c1142661f6e63d78f2266
Trang Ha <diemtrang@evol.vn>
2023-06-12 10:33:28 +0700

    feat: BEIN-15287 add permission key

---
+++ src/group.ts
@@ -0,0 +1,7 @@
+export enum JOIN_STATUS {
+  CAN_NOT_JOIN = 0,
+  CAN_JOIN = 1,
+  JOINED = 2,
+  WAITING = 3,
+  INVITED = 4,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-export enum JOIN_STATUS {
-  CAN_NOT_JOIN = 0,
-  CAN_JOIN = 1,
-  JOINED = 2,
-  WAITING = 3,
-}
-
-export * from "./permission";
-export * from "./shared.dto";
-export * from "./user-report";
+export * from './permission';
+export * from './shared.dto';
+export * from './user-report';
